25c9ed63f48ab7c52032bcca9825b8981d4b7f01
Neel Adwani <nprosofficial@gmail.com>
2022-01-10 14:28:00 +0000

    added function for jsonification

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask('app')
+
+mode = "w"
 
 @app.route('/')
 def hello_world():
   return 'Hello, World!'
+
+@app.route('/drive', methods = ['GET'])
+def drive():
+  return jsonify(mode)
 
 app.run(host='0.0.0.0', port=8080)
